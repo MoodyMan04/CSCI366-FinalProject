353de7f1de89a19a2f1d1dff8e4149738df2d600
Mason Myles <mason.myles98@gmail.com>
2023-11-28 13:49:41 -0600

    database

--- CSCI336-FinalProject/CSCI366FinalWork/Database.cs
+++ CSCI336-FinalProject/CSCI366FinalWork/Database.cs
@@ -5,6 +5,22 @@ namespace CSCI336_FinalProject.CSCI366FinalWork
     public class Database
     {
         private static string connString = "Host=localhost;Database=ACMLibrary;Username=postgres;Password=***********;Persist Security Info=True;Port=5433";
-        NpgsqlConnection db = new NpgsqlConnection();
+        NpgsqlConnection dbConn = new NpgsqlConnection(connString);
+
+        private static NpgsqlDataReader example(string sqlComman)
+        {
+            NpgsqlConnection dbConn = new NpgsqlConnection(connString);
+            dbConn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand(sqlComman, dbConn);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+        private void openConnection(NpgsqlConnection dbConn)
+        {
+            // below is how you connect
+            dbConn.Open();
+            // below is how to close
+            dbConn.Close();
+        }
     }
 }
--- CSCI336-FinalProject/CSCI366FinalWork/DatabaseManager.cs
+++ CSCI336-FinalProject/CSCI366FinalWork/DatabaseManager.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSCI336_FinalProject.CSCI366FinalWork
+﻿namespace CSCI336_FinalProject.CSCI366FinalWork
 {
-    internal interface Interface1
+    internal interface DatabaseManager
     {
+
     }
 }
