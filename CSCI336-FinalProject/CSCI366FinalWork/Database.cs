aad900b3520f5c7ebbbf743c767383a4f4b87aa0
Mason Myles <mason.myles98@gmail.com>
2023-11-28 13:46:50 -0600

    database and interface

---
+++ CSCI336-FinalProject/CSCI366FinalWork/Database.cs
@@ -0,0 +1,10 @@
+﻿using Npgsql;
+
+namespace CSCI336_FinalProject.CSCI366FinalWork
+{
+    public class Database
+    {
+        private static string connString = "Host=localhost;Database=ACMLibrary;Username=postgres;Password=***********;Persist Security Info=True;Port=5433";
+        NpgsqlConnection db = new NpgsqlConnection();
+    }
+}
