1ab13200b7bf5e6bea2bcc57a2da8d91d13dcdc2
Gunnar Moody <moodygman04@gmail.com>
2023-11-29 23:11:46 -0600

    Update Book.cs
    fixed:
    - added comments to mark untested methods
    - query statements to reflect database
    
    added:
    - static sql method to return book by id, returning a single book

--- CSCI336-FinalProject/CSCI366FinalWork/Objects/Book.cs
+++ CSCI336-FinalProject/CSCI366FinalWork/Objects/Book.cs
@@ -21,16 +21,16 @@ namespace CSCI336_FinalProject.CSCI366FinalWork.Objects
 
         // Static method to get a list of all books (called by webpage)
         // (Example of sql method, returning a list of book objects)
-        public static List<Book> GetBooksAll()
+        public static List<Book> GetBooksAll() // UNTESTED
         {
             // Get db connection
             NpgsqlConnection conn = DatabaseManager.GetConnection();
 
-            // Make command for db
-            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM Book ORDER BY Title", conn);
-
             // Open connection to db
             conn.Open();
+
+            // Make command for db
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM Books ORDER BY title", conn);
 
             // Run command and make list of books
             List<Book> books = new List<Book>();
@@ -48,11 +48,45 @@ namespace CSCI336_FinalProject.CSCI366FinalWork.Objects
                 books.Add(book);
             }
 
-            // close db connection
+            // Close db connection
             conn.Close();
 
-            // return list of books
+            // Return list of books
             return books;
         }
+
+        // Static method to get book with matching id (called by webpage)
+        // (Example of prepared sql command, returning a book)
+        public Book GetBookById(int Book_id) // UNTESTED
+        {
+            // Get db connection
+            NpgsqlConnection conn = DatabaseManager.GetConnection();
+
+            // Open connection to db
+            conn.Open();
+
+            // Make command for db
+            string query = "SELECT * FROM Books WHERE Book_id = @Book_id";
+            NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@Book_id", Book_id);
+            cmd.Prepare();
+
+            // Get data out of command and get book object
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+
+            Book book = new Book();
+            book.Book_id = Convert.ToInt32(reader["Book_id"]);
+            book.title = Convert.ToString(reader["title"]);
+            book.publisher = Convert.ToString(reader["publisher"]);
+            book.dev_language = Convert.ToString(reader["dev_language"]);
+            book.date_published = Convert.ToDateTime(reader["date_published"]);
+
+            // Close connection to db
+            conn.Close();
+
+            // Return book
+            return book;
+        } 
     }
 }
