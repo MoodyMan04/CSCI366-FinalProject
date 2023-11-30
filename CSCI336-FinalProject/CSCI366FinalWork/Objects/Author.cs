62dc186bf78a1d7003904a77846047918a6d25a4
Gunnar Moody <115500822+MoodyMan04@users.noreply.github.com>
2023-11-30 12:33:08 -0600

    Object classes (#18)
    * base database manager and book object
    
    added:
    - book object with instance variables and example method
    - folders to work folder with objects and webpages
    
    changed:
    - datamanager to have connString and public method to get connection
    
    * Update Book.cs
    
    fixed:
    - added comments to mark untested methods
    - query statements to reflect database
    
    added:
    - static sql method to return book by id, returning a single book
    
    * object classes added
    
    added:
    - base of author class (instance variables)
    - base of class class (instance variables)
    - base of user class (instance variables)
    * all will need sql methods soon *
    
    changed:
    - book class
        - changed GetBookById to static method
        - added comments to mark where rest of sql methods go for book class
    
    * Update CSCI336-FinalProject.csproj

---
+++ CSCI336-FinalProject/CSCI366FinalWork/Objects/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSCI336_FinalProject.CSCI366FinalWork.Objects
+{
+    public class Author
+    {
+        // Instance variables
+        public int Author_id { get; private set; }
+        public string first_name { get; private set; }
+        public string last_name { get; private set; }
+
+        // SQL Methods
+        // REST OF SQL METHODS FOR AUTHOR CLASS ADDED HERE
+    }
+}
---
+++ CSCI336-FinalProject/CSCI366FinalWork/Objects/Book.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using Npgsql;
+
+namespace CSCI336_FinalProject.CSCI366FinalWork.Objects
+{
+    public class Book
+    {
+        // Instance variables
+        public int Book_id { get; private set; }
+        public string title { get; private set; }
+        public string publisher { get; private set; }
+        public string dev_language { get; private set; }
+        public DateTime date_published { get; private set; }
+
+        // SQL methods
+
+        // Static method to get a list of all books (called by webpage)
+        // (Example of sql method, returning a list of book objects)
+        public static List<Book> GetBooksAll() // UNTESTED
+        {
+            // Get db connection
+            NpgsqlConnection conn = DatabaseManager.GetConnection();
+
+            // Open connection to db
+            conn.Open();
+
+            // Make command for db
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM Books ORDER BY title", conn);
+
+            // Run command and make list of books
+            List<Book> books = new List<Book>();
+
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Book book = new Book();
+                book.Book_id = Convert.ToInt32(reader["Book_id"]);
+                book.title = Convert.ToString(reader["title"]);
+                book.publisher = Convert.ToString(reader["publisher"]);
+                book.dev_language = Convert.ToString(reader["dev_language"]);
+                book.date_published = Convert.ToDateTime(reader["date_published"]);
+
+                books.Add(book);
+            }
+
+            // Close db connection
+            conn.Close();
+
+            // Return list of books
+            return books;
+        }
+
+        // Static method to get book with matching id (called by webpage)
+        // (Example of prepared sql command, returning a book)
+        public static Book GetBookById(int Book_id) // UNTESTED
+        {
+            // Get db connection
+            NpgsqlConnection conn = DatabaseManager.GetConnection();
+
+            // Open connection to db
+            conn.Open();
+
+            // Make command for db
+            string query = "SELECT * FROM Books WHERE Book_id = @Book_id";
+            NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@Book_id", Book_id);
+            cmd.Prepare();
+
+            // Get data out of command and get book object
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+
+            Book book = new Book();
+            book.Book_id = Convert.ToInt32(reader["Book_id"]);
+            book.title = Convert.ToString(reader["title"]);
+            book.publisher = Convert.ToString(reader["publisher"]);
+            book.dev_language = Convert.ToString(reader["dev_language"]);
+            book.date_published = Convert.ToDateTime(reader["date_published"]);
+
+            // Close connection to db
+            conn.Close();
+
+            // Return book
+            return book;
+        } 
+
+        // REST OF SQL METHODS FOR BOOK CLASS ADDED HERE
+    }
+}
