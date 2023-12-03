3ff7159ec38ebf486335fe695f7f61ae76e980ed
Gunnar Moody <moodygman04@gmail.com>
2023-12-03 17:21:12 -0600

    Update DatabaseManager.cs
    changed:
    - DatabaseManager:
        - removed current password
        - added note to inform to use your personal password when running database

--- CSCI336-FinalProject/CSCI366FinalWork/DatabaseManager.cs
+++ CSCI336-FinalProject/CSCI366FinalWork/DatabaseManager.cs
@@ -8,8 +8,8 @@ namespace CSCI336_FinalProject.CSCI366FinalWork
 {
     public class DatabaseManager
     {
-        // Connection string for database (HOPEFULLY THIS WORKS, PASSWORD MAY BE AN ISSUE)
-        private static string connString = "Host=localhost;Port=5433;Database=ACMLibrary;Username=postgres;Password=#Star@Man^2020;Persist Security Info=True";
+        // Connection string for database (WHEN IN USE, CHANGE PASSWORD TO YOUR LOCAL PASSWORD FOR POSTGRES)
+        private static string connString = "Host=localhost;Port=5433;Database=ACMLibrary;Username=postgres;Password=***;Persist Security Info=True";
 
         // Pulic method for opening and getting a connection
         public static NpgsqlConnection GetConnection()
