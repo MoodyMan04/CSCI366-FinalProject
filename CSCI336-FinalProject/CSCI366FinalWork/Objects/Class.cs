50b348be11a7a0575ea35cbdba5b30729fae0a74
Mason Myles <mason.myles98@gmail.com>
2023-12-06 04:00:33 -0600

    added some more methods to the work load

--- CSCI336-FinalProject/CSCI366FinalWork/Objects/Class.cs
+++ CSCI336-FinalProject/CSCI366FinalWork/Objects/Class.cs
@@ -1,8 +1,6 @@
 ﻿using Npgsql;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace CSCI336_FinalProject.CSCI366FinalWork.Objects
 {
@@ -53,5 +51,74 @@ namespace CSCI336_FinalProject.CSCI366FinalWork.Objects
                 throw new Exception(ex.Message, ex);
             }
         }
+
+        /// <summary>
+        /// fetches Class by passes in Id.
+        /// </summary>
+        /// <param name="id"> the id of the specified book </param>
+        /// <returns> a list of class objects </returns>
+        /// <exception cref="Exception"> if there is a issue with fetching class</exception>
+        public static List<Class> getClassByID(int id)
+        {
+            try
+            {
+                NpgsqlConnection conn = DatabaseManager.GetConnection();
+
+                conn.Open();
+                string query = "SELECT class_name FROM classes WHERE class_id = @ClassId";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ClassId", id);
+                cmd.Prepare();
+
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                List<Class> classes = new List<Class>();
+
+                while (reader.Read())
+                {
+                    Class csciclass = new Class();
+                    csciclass.Class_id = Convert.ToInt32(reader["Class_id"]);
+                    csciclass.class_name = Convert.ToString(reader["class_name"]);
+                    csciclass.class_description = Convert.ToString(reader["class_description"]);
+
+                    classes.Add(csciclass);
+                }
+                conn.Close();
+
+                return classes;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+        /// <summary>
+        /// Adds a class with the given class name and description parameters.
+        /// </summary>
+        /// <param name="className"> the name of the new class </param>
+        /// <param name="classDescription"> the description of the new class </param>
+        /// <exception cref="Exception"> if there was an issue inserting new class </exception>
+        public static void AddClass(string className, string classDescription)
+        {
+            try
+            {
+                NpgsqlConnection conn = DatabaseManager.GetConnection();
+                conn.Open();
+
+                string query = "INSERT INTO classes (class_name, class_description) VALUES (@className, @classDescription)";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@className", className);
+                cmd.Parameters.AddWithValue("@classDescription", classDescription);
+                cmd.Prepare();
+
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
     }
 }
